13206b56d3783e2a436e3878b1ed71db3a15f98c
nanovazquez <marianodvazquez@gmail.com>
2015-11-24 17:35:59 -0300

    Refactor index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-  (function (){
+(function (){
 
-  var methods                 = document.getElementById('method');
-  var ulrInput                = document.getElementById('url');
-  var supportsCredentialsInput = document.getElementById('supportsCredentials');
-  var customHeadersInput      = document.getElementById('custom-headers');
-  var button                  = document.getElementById('test');
-  var result                  = document.getElementById('result');
+  var methods                   = document.getElementById('method');
+  var ulrInput                  = document.getElementById('url');
+  var supportsCredentialsInput  = document.getElementById('supportsCredentials');
+  var customHeadersInput        = document.getElementById('custom-headers');
+  var button                    = document.getElementById('test');
+  var result                    = document.getElementById('result');
 
   (function init(){
+    
     ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'].forEach(function(item) {
       var option       = document.createElement('option');
       option.innerHTML = item;
       option.value     = item;
       methods.appendChild(option);
     });
-    ulrInput.value = 'http://172.16.22.242:8080/api/teams';
+
     customHeadersInput.value = 'X-PINGOTHER';
     methods.selectedIndex = 0;
     button.addEventListener('click', initCorsTest);
+  
   })();
-
-  var xhr;
 
   function initCorsTest(e){
 
@@ -29,7 +29,7 @@
     }
 
     e.preventDefault();
-    xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
     xhr.open(methods.value, ulrInput.value + '?x=' + +new Date, true);
 
@@ -47,28 +47,28 @@
     xhr.send();
   }
 
-  function showResults() {
+  function showResults(xhr) {
       result.innerHTML =
         (xhr.readyState == 4 && xhr.status == 200)
           ? xhr.responseText
           : 'ERROR! Status is "' + xhr.status + '" and status text is "' + xhr.statusText+ '"';
     };
 
-    function transferComplete(e){
-      console.log("Transfer completed");
-      console.log(e);
-      showResults();
-    }
+  function transferComplete(e){
+    console.log("Transfer completed");
+    console.log(e);
+    showResults(e.currrentTarget);
+  }
 
-    function transferFailed(e){
-      console.log("Transfer failed");
-      console.log(e);
-      showResults();
-    }
+  function transferFailed(e){
+    console.log("Transfer failed");
+    console.log(e);
+    showResults(e.currrentTarget);
+  }
 
-    function transferCanceled(e){
-      console.log("Transfer canceled");
-      console.log(e);
-      showResults();
-    }
+  function transferCanceled(e){
+    console.log("Transfer canceled");
+    console.log(e);
+    showResults(e.currrentTarget);
+  }
 })();
